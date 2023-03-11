f7b61c3b3338999261563a2fb7e764c39ee16ce8
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-03-11 20:51:15 +0100

    modif

--- mobeeci-react/src/App.jsx
+++ mobeeci-react/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./assets/pages/Home/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./assets/pages/Register/Register.jsx";
 import Login from "./assets/pages/Login/Login.jsx";
+import Suggestions from "./assets/pages/Suggestions/Suggestions.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/suggestions" element={<Suggestions />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
