eccff5863da6ab8ec731769801e897826c6901b2
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-03-11 21:22:46 +0100

    modif

--- mobeeci/resources/views/welcome.blade.php
+++ mobeeci/resources/views/welcome.blade.php
@@ -1,2 +1,27 @@
-<?php 
-echo "Page d'accueil"
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/scss/welcome.scss', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+
+<body>
+    <x-logo />
+    <a href="/login">
+        <button class="login-home">
+            <b>Login</b>
+        </button>
+    </a>
+
+    <a href="/register">
+        <button class="register-home">
+            <b>Register</b>
+        </button>
+    </a>
+</body>
+
+</html>
